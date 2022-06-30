3a7db31d83cc0facb9aa47c50761353a508e36f9
Hassan Bazzi <Bazzih3519@gmail.com>
2022-06-30 19:51:20 -0400

    1

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:4000';
+const url         = 'mongodb://localhost:27017';
 let db            = null;
 
 // connect to mongo
